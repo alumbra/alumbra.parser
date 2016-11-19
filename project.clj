69f3b6e0c525ab77330bdaf7a0fffa63d9678221
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-19 14:02:28 +0100

    make 'alumbra/spec' a provided dependency.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :year 2016
             :key "mit"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
-                 [alumbra/spec "0.1.0-SNAPSHOT"]
+                 [alumbra/spec "0.1.0" :scope "provided"]
                  [clj-antlr "0.2.3"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
                                   [alumbra/generators "0.1.1-SNAPSHOT"]]}
