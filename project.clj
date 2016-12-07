efce32f8f351f38510d6c5972789cc5c379282d2
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-12-07 13:54:13 +0100

    Version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alumbra/parser "0.1.3"
+(defproject alumbra/parser "0.1.4-SNAPSHOT"
   :description "A GraphQL parser for Clojure using ANTLR4."
   :url "https://github.com/alumbra/alumbra.parser"
   :license {:name "MIT License"
